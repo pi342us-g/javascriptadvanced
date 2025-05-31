12e9f16a6faa4e15c032d14a68028a0454d45d31
pi342us-g <ndubipius96@gmail.com>
2025-05-31 12:37:22 +0300

    assignment

---
+++ Assignment3b.js
@@ -0,0 +1,14 @@
+//let rawName = "   diana karanja  ";
+// let rawEmail = "DianaKaranja@Email.com   ";
+// let course = "Introduction to coding";
+// let age1 = 17;
+
+// Assignment:
+// Welcome, Diana Karanja!
+// Email: dianakaranja@email.com
+// Course: INTRODUCTION TO CODING
+// Your email address has 25 characters.
+
+// First name: Diana
+// Updated course: Introduction to JavaScript
+// Initials: D.K.
