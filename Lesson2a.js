665c372bc12005b6112490be8be34b0e44097714
pi342us-g <ndubipius96@gmail.com>
2025-05-31 11:02:01 +0300

    added file

--- Lesson2a.js
+++ Lesson2a.js
@@ -8,3 +8,23 @@ console.log("The fruit on index number 2 is :" + fruits[2])
 // we use it to add an item at the end of array
 fruits.push("Apple")
 console.log(fruits)
+
+// method 2 slice method
+// used to output from a given index to another
+let somefruits = fruits.slice(1,2)
+console.log(somefruits)
+
+// pop method - we use it to remove item of an array that apears at thee end
+fruits.pop()
+fruits.pop()
+console.log(fruits)
+
+// length method - used to determine the number of items of  array
+console.log("The number of items in our fruits array is:" + fruits.length)
+ 
+let cities = ["Nairobi","Mombasa","Nakuru","Eldoret","Meru","Machakos"];
+
+// the filter method is used to show output based on some rules
+let updatedCities = cities.filter(item => item !== "Meru");
+ // it will create a new array of cities without the city meru
+ console.log(updatedCities)
