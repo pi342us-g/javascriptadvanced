5b64e6f4f482369936b49c3e3492a97ba4bc19c8
pi342us-g <ndubipius96@gmail.com>
2025-05-31 10:47:36 +0300

    added a file

---
+++ Lesson2a.js
@@ -0,0 +1,10 @@
+// array data type
+// an array is a collection of related items which are of same data type
+let fruits = ["Banana","Mangoes","Oranges","Grapes"]
+console.log("The fruit on index number 2 is :" + fruits[2])
+
+// some methods used to modify items of an array
+// method 1 = push method
+// we use it to add an item at the end of array
+fruits.push("Apple")
+console.log(fruits)
