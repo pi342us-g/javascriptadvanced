08a8586ebe1403d6cb470ced99e51bfa66b0101c
pi342us-g <ndubipius96@gmail.com>
2025-05-31 10:39:08 +0300

    first commit

---
+++ Lesson1a.js
@@ -0,0 +1,33 @@
+// string - represent text inside quotes
+let str = "Hello , Javascript"
+console.log(str)
+console.log(typeof str)
+
+// Number - represent both interger and floating point numbers
+
+let num =  42
+console.log("the number is "+ num)
+console.log(typeof num)
+
+let num2 = 42.66
+console.log("the number is "+ num2)
+console.log(typeof num2)
+
+// BigInt - used for very large intergers
+let bigInt = 734663498078n;
+console.log(typeof bigInt);
+
+// Boolean - represent true or false
+let isAvailable = true;
+let isDone = false;
+console.log(typeof isAvailable);
+console.log(isDone);
+
+// undefined - a variable declared but value not assigned
+let notAssigned;
+console.log(typeof notAssigned)
+
+// null - it represent intentional absence of any object . it evaluates as an object
+let emptyValue = null;
+console.log(typeof emptyValue)
+
---
+++ Lesson1b.js
@@ -0,0 +1,12 @@
+// Arithmetic operations
+ let a = 5
+ let b = 4
+
+ // addition
+ console.log("the sum of the two numbers is:"+a+b)
+ console.log("The product is :"+ a*b)
+ console.log("The quotient is :" + a/b)
+ console.log("The difference is :" +( a-b))
+ console.log("The remainder is :" + a%b)
+ console.log("The remainder is :" + b%a)
+console.log("Exponent of number is :" + (a**b))
