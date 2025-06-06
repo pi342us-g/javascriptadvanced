4e8d6e4d0892e34e3d69da99db5084f2dd83906c
Pius Wanyangu <ndubipius96@gmail.com>
2025-06-06 14:47:29 +0300

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,69 @@
+let i = 100;
+while (i >= 30) {
+  console.log(i);
+  i--;
+}
+//assignment 3b
+// create a js program that checks if a number entered is positive or negative or zero
+
+let number = 0;
+if (number > 0) {
+  console.log("The number is positive");
+} else if (number < 0) {
+  console.log("The number is negative");
+} else if (number === 0) {
+  console.log("The number is zero");
+} else {
+  console.log(number, "is not a number");
+}
+
+// Read and make some short notes on comparison operators in js, logical operators and assignment operators in js.
+
+// assignment 4a
+// Assignment :create a register function
+// it will have username,email,password and phone
+// check if the email is valid by checking the at symbol and dot
+// check password if is strong and finally resgister successfully
+function register(username, email, password, phone) {
+  if (email.includes("@") && email.includes(".")) {
+    if (password.length >= 8) {
+      console.log("Registration successfully");
+    } else {
+      console.log("Password must contain a minimum of 8 chars");
+    }
+  } else {
+    console.log("Your email should contain @ and . symbols! ");
+  }
+}
+
+register("Pius", "pius@gmail.com", "Dghk6mk.", "0792342944");
+
+// assignment
+// Remind yourselves on arrow functions
+// create an arrow function that adds two numbers. Call it summation.
+const summation = () => {
+  num1 = 34;
+  num2 = 12;
+  let add = num1 + num2;
+  console.log(add);
+};
+summation();
+// Assignment: Create an arrow function that is able to find the simple interest of three different individuals
+const SI =(P,R,T)=>{
+ 
+  let si = (P*R*T)/100
+  console.log(`The simple interest is : ${si}`)
+}
+SI(10000,15,5);
+SI(7500,5,3);
+SI(20500,10,3)
+
+//   Assignment use async to
+// https://official-joke-api.appspot.com/random_joke
+// Extract and print out the contents of the setup and the punchline
+
+// https://sengi2025.pythonanywhere.com/api/getproducts
+// On the above api endpoint extract the product_name, and the price and print them out
+
+//  Assignment: Create a button in HTML that changes a paragraph's text when clicked using
+//JavaScript
--- Assignment3b.js
+++ Assignment3b.js
@@ -2,6 +2,22 @@
 // let rawEmail = "DianaKaranja@Email.com   ";
 // let course = "Introduction to coding";
 // let age1 = 17;
+let rawName = "  diana karanja  ";
+let rawEmail = "DianaKaranja@gmail.com  ";
+let course = "Introduction to coding";
+let newName = rawName.trim().charAt(0).toUpperCase();
+let greetings = `Welcome, ${newName}!`
+console.log(greetings)
+
+let email = rawEmail.trim().toLowerCase();
+console.log("Email: " + email);
+
+let newCourse = course.toUpperCase();
+console.log("Course: " + newCourse)
+
+let emaillength = email.length;
+
+console.log("Your email address has " + emaillength + " Characters")
 
 // Assignment:
 // Welcome, Diana Karanja!
@@ -12,3 +28,84 @@
 // First name: Diana
 // Updated course: Introduction to JavaScript
 // Initials: D.K.
+let student = {
+    firstName:"Diana ",
+    lastName:"Karanja",
+    updatedCourse:"Introduction to Javascript"
+
+}
+let{firstName,updatedCourse,lastName}=student;
+console.log(firstName);
+console.log(updatedCourse);
+console.log("Initials:",firstName.charAt(0),".",lastName.charAt(0),".")
+
+
+//Assignment: create two object of a player a car
+
+let player ={
+    username:"Matineli",
+    age:21,
+    team:"Arsenal",
+    country:"UK",
+    nationality:"white"
+
+}
+console.log(player)
+
+let car ={
+    carname:"Landcruiser Prado",
+    model:"2021",
+    make:"Toyota",
+    color:"Black",
+    owner:"Pius Wanyangu"
+
+}
+console.log(car)
+
+// Assignment
+//Create an array of five numbers. use map to double them and filter to extract numbers greater than 5
+
+let numbers = [4,1,19,20,36]
+let newnumber = numbers.map(n =>(n*2)).filter(n=>n>5)
+console.log(newnumber)
+// Use map() method to convert an array of prices with 16% value added tax VAT  and show the final cost
+let prices = [500,250,300,100]
+let newprices = prices.map(p =>(p*16/100))
+// tatal VAT
+ 
+
+// find out how to replace multiple occurence
+let text = "I will meet Pius and Mike tomorrow morning. We will be travelling with Pius and Mike to ukwala , Pius will be head of State";
+text = text.replaceAll("Pius","Jeff");
+text = text.replaceAll("Mike","Hellen");
+console.log(text)
+
+// Assignment create an object product with properties id name cost quantity and description. 
+let shoe = {
+    id:2,
+    name:"Safari boots",
+    cost: 3000,
+    quantity: 45,
+    description:"Leather brown in color"
+}
+//use destructuring to extract and log them out
+let {name,cost,description}=shoe;
+console.log(name);
+console.log(cost)
+console.log(description)
+
+
+
+
+//Step1: create a list of 5 countries
+let countries = ["USA","China","Kuwait","Kenya","Egypt"]
+//step2 : use slice() to get a portion of the list
+let slicedcountries = countries.slice(1, 4);
+console.log(slicedcountries);
+// step 3: use filter() to get countries with names longer than 5 characters
+let length = 5;
+let filtered = countries.filter(n => n.length>=length)
+console.log(filtered)
+
+
+
