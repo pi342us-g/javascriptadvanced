35c06592f3e456fe6b1122c0334d0b9e4880f308
pi342us-g <ndubipius96@gmail.com>
2025-06-07 10:01:09 +0300

    new

---
+++ Lesson3b.js
@@ -0,0 +1,19 @@
+// compound assignments
+let x=5;
+
+x+=2;
+console.log("The new value of x is: "+x)
+
+x *=3;
+console.log("The answer is : "+ x)
+
+// increement and decrement
+let y = 1;
+y++;
+console.log(y)
+++y
+console.log(y)
+
+// decrement
+y--
+console.log(y)
