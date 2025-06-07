2adb64042df1e22b4bc8cb4755525dc6253fb312
pi342us-g <ndubipius96@gmail.com>
2025-06-07 10:12:06 +0300

    if statements

---
+++ Lesson3c.js
@@ -0,0 +1,24 @@
+// control flow statements
+let age = 21;
+if(age>=18){
+    console.log("You are an adult")
+}else{
+    console.log("You are a minor")
+}
+
+// if else if statements
+// it is used to evaluate multiple conditions. It returns true if a condition is met
+let grade =76;
+
+if (grade > 100){
+    console.log("Invalid grade input")
+}
+else if (grade >=90 && grade <=100){
+    console.log("The grade gotten is : A")
+}
+else if (grade >80 && grade <90){
+    console.log("The grade gotten is :B")
+}
+else{
+    console.log("Grade C and below")
+}
